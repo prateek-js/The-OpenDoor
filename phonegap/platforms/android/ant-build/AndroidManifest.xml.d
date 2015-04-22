aa228da6c09cce306807e0ef7ef8db8eeba4d567
Desai, Prateek <prateek.desai@accenture.com>
2015-04-22 11:31:28 +0530

    project structure

---
+++ phonegap/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/prateek.desai/Desktop/MyApp/TheOpenDoorTest/phonegap/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/prateek.desai/Desktop/MyApp/TheOpenDoorTest/phonegap/platforms/android/AndroidManifest.xml \
